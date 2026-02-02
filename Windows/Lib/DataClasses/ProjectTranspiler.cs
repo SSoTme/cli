80e645eb98fd820d8b942b24aff2c00cb6b2bbd1
feelixs <michael@ssot.me>
2026-02-02 10:44:05 -0500

    preserve keys during ssotme install

--- Windows/Lib/DataClasses/ProjectTranspiler.cs
+++ Windows/Lib/DataClasses/ProjectTranspiler.cs
@@ -27,13 +27,6 @@ namespace SSoTme.OST.Lib.DataClasses
         [JsonIgnore]
         public SSoTmeCLIHandler CLIHandler { get; private set; }
 
-        // Fields used by the my.effortlessapi tool marketplace that should get preserved during builds
-        // CLI doesn't use these fields, just preserves if they exist
-        public string MarketToolName { get; set; }
-        public string MarketToolVersion { get; set; }
-        public string LastUsed { get; set; }
-        public string NumExecutions { get; set; }
-
         public ProjectTranspiler()
         {
             this.InitPoco();
